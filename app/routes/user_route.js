bb10a2117eb8413f65bfd484283767797ca58757
Alex Kiura <alex.kiura@andela.com>
2016-03-03 19:51:34 +0300

    (Feature) Add post user route

---
+++ app/routes/user_route.js
@@ -0,0 +1,29 @@
+var User = require('../models/user_schema').UserModel;
+var Resolution = require('../models/user_schema').ResolutionModel;
+var uuid = require('node-uuid');
+var supersecret = 'api_v1$randomsecretcode';
+
+exports.postUser = function(req, res) {
+  var user = new User();
+  user.name = req.body.name;
+  user.username = req.body.username;
+  user.password = req.body.password;
+
+  // save user
+  user.save(function(err) {
+    if (err) {
+      // duplicate entry
+      if (err.code = 11000) {
+        return res.json({
+          success: false,
+          message: 'A user with that username already exists.'
+        });
+      } else {
+        return res.send(err);
+      }
+    }
+    res.json({
+      message: 'user successfully created.'
+    });
+  });
+};
