d7ca4d5df8b44ca8e6d4c1c45bcac7150c05ddbb
Alex Kiura <alex.kiura@andela.com>
2016-03-03 17:05:44 +0300

    (Chore) Prepare API route

--- server.js
+++ server.js
@@ -1,15 +1,43 @@
 var express = require("express");
 var app = express();
 var exports = module.exports = {};
+var router = express.Router();
+
+app.use(express.static(path.join(__dirname, "public")));
+
+//log all requests 
+app.use(morgan("dev"));
 
 // index
-app.get("/", function(req, res) {
-  res.send("API");
+router.get("/", function(req, res) {
+  res.json({
+    API: "Resolution Tracker",
+    version: "1",
+    author: "github.com/andela-akiura",
+    issues: "https://github.com/andela-akiura/resolution-tracker/issues"
+  });
 });
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.use(bodyParser.json());
+
+// handle CORS 
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST");
+  res.setHeader("Access-Control-Allow-Headers", "X-Requested-With, content-type, Authorization");
+  next();
+});
+
+// all routes follow from the api :)
+app.use("/api", router);
 
 //start server
 var server = app.listen(3000, function() {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
 
 exports.closeServer = function() {
